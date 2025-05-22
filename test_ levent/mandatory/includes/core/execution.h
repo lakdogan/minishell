57c917d2d9cf3d30c3452c7e9cafe5fff1234836
lakdogan <levent_akdogan@live.de>
2025-05-22 18:48:07 +0200

    Implement core error handling and file descriptor utilities - and some constants

--- test_ levent/mandatory/includes/core/execution.h
+++ test_ levent/mandatory/includes/core/execution.h
@@ -15,6 +15,35 @@
 # define EXECUTION_H
 
 # include "minishell.h"
+# define BUFFER_SIZE_TEE 4096
+# define FORK_ERROR -1
+# define CHILD_PROCESS 0
+# define WAIT_BLOCK 0
+# define INVALID_FD -1
+# define CMD_NOT_FOUND 127
+# define PERMISSION_DENIED 126
+# define HEREDOCS_PREPARED 1
+# define HEREDOCS_NOT_PREPARED 0
+# define VALIDATION_SUCCESS 1
+# define VALIDATION_FAILURE 0
+# define DEFAULT_FILE_PERMISSION 0644
+# define WRITE_ERROR -1
+# define OPERATION_FAILURE 0
+# define OPERATION_SUCCESS 1
+# define PIPE_WRITE_END 1
+# define PIPE_READ_END 0
+# define STRINGS_EQUAL 0
+# define GENERAL_ERROR 1
+# define SIGNAL_EXIT_BASE 128
+# define FALSE 0
+# define CLOSE_ERROR -1
+# define ABSOLUTE_PATH_CHAR '/'
+# define RELATIVE_PATH_CHAR '.'
+# define PATH_SEPERATOR '/'
+# define ACCESS_OK 0
+# define SYSCALL_ERROR -1
+# define NULL_TERMINATOR '\0'
+# define SIG_NO_FLAGS 0
 
 typedef enum e_in_type
 {
@@ -50,6 +79,8 @@ typedef struct s_exec
 	char				**argv;		// Arguments
 	t_infile			*infiles;	// Input redirections
 	t_outfile			*outfiles;	// Output redirections
+	int					heredoc_fd;
+	bool				heredoc_prepared;
 }						t_exec;
 
 #endif

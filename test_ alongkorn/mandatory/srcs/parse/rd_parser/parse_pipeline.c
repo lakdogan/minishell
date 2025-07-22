9ecc23df45502d63455eb8f5a95d876be09c030f
Aleio13 <alongkornmatschiner@gmail.com>
2025-07-23 01:41:48 +0200

    added: tested parse pipeline part of the rd parser.

---
+++ test_ alongkorn/mandatory/srcs/parse/rd_parser/parse_pipeline.c
@@ -0,0 +1,39 @@
+
+#include "../../../includes/core/minishell.h"
+
+t_command_tree	*get_node(t_node_type type, t_command_tree *left, t_command_tree *right,
+				t_gc *gc)
+{
+	t_command_tree	*new_node;
+
+	new_node = gc_malloc(gc, sizeof(t_command_tree));
+	if (!new_node)
+		return (NULL);
+	new_node->type = type;
+	new_node->left = left;
+	new_node->right = right;
+	new_node->data = NULL;
+	return (new_node);
+}
+
+t_command_tree	*parse_pipeline(t_tokens *tokens, int *pos, t_gc *gc)
+{
+	t_command_tree	*node;
+	t_command_tree	*right;
+	t_command_tree	*left;
+
+	left = parse_sub_or_cmd(tokens, pos, gc);
+	if (!left)
+		return (NULL);
+	while ((*pos) < tokens->count && tokens->arr[(*pos)].type == PIPE)
+	{
+		(*pos)++;
+		right = parse_sub_or_cmd(tokens, pos, gc);
+		if (!right)
+			return (NULL);
+		node = get_node(N_PIPE, left, right, gc);
+		left = node;
+	}
+	return (left);
+}
+

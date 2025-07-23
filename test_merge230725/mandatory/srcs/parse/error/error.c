b908acd0e70cf9d8c6573137054876332b3e0355
Aleio13 <alongkornmatschiner@gmail.com>
2025-07-24 01:17:43 +0200

    fix: better error message

--- test_merge230725/mandatory/srcs/parse/error/error.c
+++ test_merge230725/mandatory/srcs/parse/error/error.c
@@ -3,10 +3,10 @@
 
 void	error_msg(t_tokens *tokens, int *pos)
 {
-	printf("minishell: syntax error near unexpected token '%s'\n", tokens->arr[(*pos) - 1].value);
+	printf("minishell$ syntax error near unexpected token '%s'\n", tokens->arr[(*pos) - 1].value);
 }
 
 void	error_msg_paren(void)
 {
-	printf("minishell: syntax error: unexpected end of file");
+	printf("minishell$ syntax error: unexpected end of file\n");
 }

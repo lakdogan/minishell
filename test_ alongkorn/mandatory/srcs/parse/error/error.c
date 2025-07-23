402a4ce2ea8a4a7bfa021c3eb24a8e8515bf6022
Aleio13 <alongkornmatschiner@gmail.com>
2025-07-23 05:40:56 +0200

    added: error messages after parsing failed

---
+++ test_ alongkorn/mandatory/srcs/parse/error/error.c
@@ -0,0 +1,12 @@
+
+#include "../../../includes/core/minishell.h"
+
+void	error_msg(t_tokens *tokens, int *pos)
+{
+	printf("minishell: syntax error near unexpected token '%s'\n", tokens->arr[(*pos) - 1].value);
+}
+
+void	error_msg_paren(void)
+{
+	printf("minishell: syntax error: unexpected end of file");
+}
